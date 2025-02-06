d71b58e77d29ed3e1c5c8aa036a26961d9f5ac27
Michael Kelly <mike@thegrumpydev.com>
2025-02-06 15:19:23 -0800

    Update debian_setup.sh

--- debian_setup.sh
+++ debian_setup.sh
@@ -1,6 +1,12 @@
+# Log in as root and make user sudo
+usermod -aG sudo user
+groups user
+
+# Auto updates
 sudo apt install ntp -y
 sudo systemctl restart ntp
 sudo apt update && sudo apt upgrade -y
 
+# Flatpaks
 sudo apt install unattended-upgrades apt-listchanges extremetuxracer flatpak gnome-software gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
